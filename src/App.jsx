22ee67f1e2fdd81fc1adec9be85a0efb47ff0d1b
Mikael Escolin <mikaelescolin@gmail.com>
2020-11-24 23:36:22 +0000

    Fix eslint, prettier and tailwindcss configuration

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import './assets/styles/tailwind.output.css';
+
+function App() {
+  return <div className="App w-15 h-10 bg-blue-400">HELLO WORLD</div>;
+}
+
+export default App;
