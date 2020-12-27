bffcfa8d83e681265fdb9dc6e7704617ba7cdf25
Mikael Escolin <mikaelescolin@gmail.com>
2020-12-27 21:55:04 +0000

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import Header from './header/index';
+
 import './assets/styles/tailwind.output.css';
 
 function App() {
-  return <div className="App w-15 h-10 bg-blue-400">HELLO WORLD</div>;
+  return (
+    <div className="flex flex-col">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
