d09b4a950041655daf21a4bb90a1f9006cbe0322
Mikael Escolin <mikaelescolin@gmail.com>
2020-12-28 21:13:59 +0000

    Add sources component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './header/index';
+import Sources from './sources/index';
 
 import './assets/styles/tailwind.output.css';
 
@@ -8,6 +9,9 @@ function App() {
   return (
     <div className="flex flex-col">
       <Header />
+      <main className="flex">
+        <Sources />
+      </main>
     </div>
   );
 }
---
+++ src/sources/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const sources = () => {
+  return (
+    <div className="bg-gray-100 h-screen w-1/4">
+      <ul className="flex flex-col h-full overflow-scroll text-center">
+        <li className="mt-6">Source 1</li>
+        <li className="mt-6">Source 2</li>
+        <li className="mt-6">Source 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default sources;
