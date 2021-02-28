54a835dd007e22017d206b0fab7f67dc7e8d3f4c
Mikael Escolin <mikaelescolin@gmail.com>
2021-02-28 17:00:12 +0000

    Reset file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 
-import Header from './header/index';
-import News from './news/index';
-import Sources from './sources/index';
-
-import './assets/styles/tailwind.output.css';
-
 function App() {
   return (
-    <div className="flex flex-col h-screen">
-      <Header />
-      <main className="flex flex-grow">
-        <Sources />
-        <News />
-      </main>
+    <div>
+      <h1>Hello, World!</h1>
     </div>
   );
 }
