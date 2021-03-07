b2ef11dba6f41e6e909aa54af25a1be0983f584f
Mikael Escolin <mikaelescolin@gmail.com>
2021-03-07 14:34:28 +0000

    Update News Component to have shorter first row

--- src/components/news/News.view.js
+++ src/components/news/News.view.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid } from '@chakra-ui/react';
 
 const NewsView = () => (
-  <Grid templateRows="1fr 4fr" gap="0" h="100%">
+  <Grid templateRows="5rem 4fr" gap="0" h="100%">
     <div>hello</div>
     <div>hi</div>
   </Grid>
