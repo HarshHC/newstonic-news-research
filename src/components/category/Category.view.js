3c6f099ece4bde98f64dc6aac3609791957811a0
Mikael Escolin <mikaelescolin@gmail.com>
2021-03-07 15:00:53 +0000

    Add box shadows and padding

--- src/components/category/Category.view.js
+++ src/components/category/Category.view.js
@@ -3,7 +3,7 @@ import { Flex } from '@chakra-ui/react';
 import CategorySelect from './category-select';
 
 const CategoryView = () => (
-  <Flex w="100%" h="100%" bg="blue.100" alignItems="center" px="2rem">
+  <Flex w="100%" h="100%" alignItems="center" px="2rem" boxShadow="base">
     <CategorySelect>
       <option value="cat 1">cat 1</option>
       <option value="cat 2">cat 2</option>
--- src/components/layout/header/Header.view.js
+++ src/components/layout/header/Header.view.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Center, Heading } from '@chakra-ui/react';
 
 const HeaderView = ({ text }) => (
-  <Center>
+  <Center borderBottom="1px solid rgba(0, 0, 0, .1)" py="2">
     <Heading as="h1">{text}</Heading>
   </Center>
 );
