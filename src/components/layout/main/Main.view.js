0ffc52de5d16faa809c979d797ff784e60987a74
Mikael Escolin <mikaelescolin@gmail.com>
2021-03-07 14:33:24 +0000

    Add News to main

--- src/components/layout/main/Main.view.js
+++ src/components/layout/main/Main.view.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Grid } from '@chakra-ui/react';
 import Sources from '../../sources';
+import News from '../../news';
 
 const MainView = () => (
   <Grid w="100%" h="100%" templateColumns="1fr 2fr">
     <Sources />
-    <div>hello</div>
+    <News />
   </Grid>
 );
 
